f08aa3c6a4e67a58ff4e910eb4485f83a306004a
SantiagooAguerre <santiago.aguerre@correo.ucu.edu.uy>
2025-05-11 19:23:49 -0300

    cambios

--- UT3_PD3/src/paquete/Lista.java
+++ UT3_PD3/src/paquete/Lista.java
@@ -1,94 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package paquete;
 
-public abstract class Lista<T> implements ILista<T> {
+public class Lista<T> implements ILista<T> {
+
     private Nodo<T> primero;
-    
+
     public Lista() {
         primero = null;
     }
-    
+
+    @Override
+    public void insertar(Nodo nodo) {
+        if (primero == null) {
+            primero = nodo;
+        } else {
+            Nodo<T> nodos = primero;
+            while (nodos != null) {
+                nodos = nodos.getSiguiente();
+            }
+            nodos.setSiguiente(nodo);
+        }
+    }
+
+    @Override
+    public void insertar(Comparable etiqueta, T dato) {
+        Nodo<T> nodoNuevo = new Nodo<>(dato, etiqueta);
+        insertar(nodoNuevo);
+    }
+
+    @Override
+    public Nodo buscar(Comparable clave) {
+        Nodo<T> nodo = primero;
+        while (nodo != null) {
+            if (nodo.getEtiqueta().equals(clave)) {
+                return nodo;
+            }
+            nodo = nodo.getSiguiente();
+        }
+        return null;
+
+    }
+
+    @Override
+    public boolean eliminar(Comparable clave) {
+        Nodo<T> nodo = primero;
+        Nodo<T> anterior = null;
+        while (nodo != null) {
+            if (nodo.getEtiqueta().equals(clave)) {
+                if (anterior == null) {
+                    primero = nodo.getSiguiente();
+                } else {
+                    anterior.setSiguiente(nodo.getSiguiente());
+                }
+                return true;
+            }
+            anterior = nodo;
+            nodo = nodo.getSiguiente();
+        }
+        return false;
+    }
+
+    @Override
+    public String imprimir() {
+        if (primero == null) {
+            return "";
+        }
+        StringBuilder info = new StringBuilder();
+        Nodo<T> nodo = primero;
+        while (nodo != null) {
+            info.append(nodo.getEtiqueta()).append(" -> ");
+            nodo = nodo.getSiguiente();
+        }
+        return info.toString();
+    }
+
+    @Override
+    public String imprimir(String separador) {
+        if (primero == null) {
+            return "";
+        }
+        StringBuilder info = new StringBuilder();
+        Nodo<T> nodo = primero;
+        while (nodo != null) {
+            info.append(nodo.getEtiqueta());
+            if (nodo.getEtiqueta() != null) {
+                info.append(separador);
+            }
+            nodo = nodo.getSiguiente();
+        }
+        return info.toString();
+    }
+
+    @Override
+    public int cantElementos() {
+        Nodo<T> nodo = primero;
+        int contador = 0;
+        while (nodo != null) {
+            contador++;
+            nodo = nodo.getSiguiente();
+        }
+        return contador;
+    }
+
     @Override
     public boolean esVacia() {
         return primero == null;
     }
-    
+
     @Override
-    public void setPrimero(Nodo<T> unNodo) {
+    public void setPrimero(Nodo unNodo) {
         primero = unNodo;
     }
-    
-    @Override
-    public void insertar(Comparable etiqueta, T dato) {
-        Nodo<T> nuevoNodo = new Nodo<>(etiqueta, dato);
-        insertar(nuevoNodo);
-    }
 
-    @Override
-    public void insertar(Nodo<T> nodo) {
-        if (primero == null) {
-            primero = nodo;
-        } else {
-            Nodo<T> temp = primero;
-            while (temp != null) {
-                temp = temp.getSiguiente();
-            }
-            temp.setSiguiente(nodo);
-        }
-
-    }
-
-    @Override
-    public Nodo<T> buscar(Comparable clave) {
-        Nodo<T> temp = primero;
-        while (temp != null) {
-            if (temp.getEtiqueta().compareTo(clave) == 0) {
-                return temp;
-            }
-            temp=temp.getSiguiente();
-        }
-        return null;
-    }
-    
-    @Override
-    public String imprimir() {
-        StringBuilder sb = new StringBuilder();
-        Nodo<T> temp = primero;
-        while (temp != null) {
-            sb.append(temp.getEtiqueta().toString()).append(" ");
-            temp = temp.getSiguiente();
-        }
-        return sb.toString();
-    }
-    
-    @Override
-    public int cantElementos() {
-        int contador = 0;
-        Nodo<T> temp = primero;
-        while (temp != null) {
-            contador++;
-            temp = temp.getSiguiente();
-        }
-        return contador;
-    }
-    @Override
-    public boolean eliminar(Comparable clave) {
-        Nodo<T> temp = primero;
-        Nodo<T> previo = null;
-        while (temp != null) {
-            if (temp.getEtiqueta().compareTo(clave) == 0) {
-                if (previo == null) {
-                    primero = temp.getSiguiente();
-                } else {
-                    previo.setSiguiente(temp.getSiguiente());
-                }
-                return true;
-            }
-            previo = temp;
-            temp = temp.getSiguiente();
-        }
-        return false;
-    }
-    
-    
-    
 }
--- UT3_PD3/src/paquete/Nodo.java
+++ UT3_PD3/src/paquete/Nodo.java
@@ -1,15 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package paquete;
 
-public  class Nodo<T> implements INodo<T> {
+/**
+ *
+ * @author Estudiante UCU
+ */
+public class Nodo<T> implements INodo<T> {
     private T dato;
     private Nodo<T> siguiente;
     private Comparable etiqueta;
-    
-    public Nodo(Comparable etiqueta, T dato) {
-        this.etiqueta = etiqueta;
+
+    public Nodo(T dato, Comparable etiqueta) {
         this.dato = dato;
         this.siguiente = null;
+        this.etiqueta = etiqueta;
     }
+    
     
     @Override
     public T getDato() {
@@ -17,22 +26,22 @@ public  class Nodo<T> implements INodo<T> {
     }
 
     @Override
-    public Nodo<T> getSiguiente(){
+    public Nodo<T> getSiguiente() {
         return siguiente;
     }
 
     @Override
-    public void setSiguiente(Nodo<T> nodo){
+    public void setSiguiente(Nodo<T> nodo) {
         this.siguiente = nodo;
     }
 
     @Override
-    public void imprimir(){
+    public void imprimir() {
         System.out.println(dato);
     }
 
     @Override
-    public void imprimirEtiqueta(){
+    public void imprimirEtiqueta() {
         System.out.println(etiqueta);
     }
 
@@ -42,9 +51,8 @@ public  class Nodo<T> implements INodo<T> {
     }
 
     @Override
-    public int compareTo(Comparable etiqueta){
+    public int compareTo(Comparable etiqueta) {
         return this.etiqueta.compareTo(etiqueta);
-
     }
-
+    
 }
